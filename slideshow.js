e1ddfebb1497f945defc603ea15308409d12a9ca
samikassimaki <sami.kassimaki@aalto.fi>
2017-03-06 14:58:34 +0200

    OK

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,3 @@
-$.getJSON("https://kassims1-a8fe3.firebaseio.com/.json",function(data) { obj = console.log(data)});
+$.getJSON("https://kassims1-a8fe3.firebaseio.com/.json",function(data) { console.log(data)});
 
 
